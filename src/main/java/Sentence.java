f4a73552996cb7864b3b571b65f9d1c49ed5b233
Roman Kaminskiy - pc <derrki@gmail.com>
2018-06-19 00:32:10 +0300

    2. Написано простий тест, котрий перевіряє створення речення з одного слова. Створено клас, що забезпечує проходження тесту. (Як тест так і клас потребують глубокого рефакторингу !)

---
+++ src/main/java/Sentence.java
@@ -0,0 +1,15 @@
+/**
+ * 1. Make it work. 2. Make it right. 3. Make it fast
+ */
+public class Sentence {
+
+    private String word;
+
+    public Sentence(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
---
+++ src/test/java/TestConstructionOfASentence.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * 1. Make it work. 2. Make it right. 3. Make it fast
+ */
+public class TestConstructionOfASentence {
+
+    @Test
+    public void test_build_a_sentence_with_one_word(){
+
+        Sentence sentence = new Sentence("go");
+        String simpleSentence = sentence.getWord();
+        assertEquals("go", simpleSentence);
+    }
+
+}
